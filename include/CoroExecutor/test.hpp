6bb1484c02f4506edec34e741eab901187f6d6b3
Caillin Nugent <caillinn@student.unimelb.edu.au>
2025-08-13 18:39:58 +1000

    CMake test

---
+++ include/CoroExecutor/test.hpp
@@ -0,0 +1,11 @@
+#ifndef TEST_H
+#define TEST_H
+
+namespace test 
+{
+    int add(int a, int b);
+} // namespace test
+
+
+
+#endif // TEST_H
---
+++ src/test.cpp
@@ -0,0 +1,12 @@
+
+#include "CoroExecutor/test.hpp"
+
+namespace test 
+{
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+} // namespace test
