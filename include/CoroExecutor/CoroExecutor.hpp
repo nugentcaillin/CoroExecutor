74f57697ce458779f01679c05bc99d95f865f13f
Caillin Nugent <caillinn@student.unimelb.edu.au>
2025-08-15 11:33:12 +1000

    removed test.cpp

--- include/CoroExecutor/CoroExecutor.hpp
+++ include/CoroExecutor/CoroExecutor.hpp
@@ -13,6 +13,40 @@
 namespace CoroExecutor
 {
 
+class CoroExecutor;
+
+class LifetimeManagedCoroutine
+{
+public:
+    struct promise_type 
+    {
+        using handle  = std::coroutine_handle<promise_type>;
+        LifetimeManagedCoroutine get_return_object() { return LifetimeManagedCoroutine(handle::from_promise(*this)); };
+        void unhandled_exception() noexcept {};
+        std::suspend_always initial_suspend() noexcept { return {}; };
+        void return_void() {};
+
+        // signal to CoroExecutor coroutine is ready for deletion
+        std::suspend_always final_suspend();
+        
+        // assigned by CoroExecutor in add_lifetime_coroutine
+        std::shared_ptr<CoroExecutor> executor;
+    };
+
+
+    LifetimeManagedCoroutine
+    ( promise_type::handle handle
+    )
+    : handle_(handle)
+    {}; 
+
+private:
+    promise_type::handle handle_;
+};
+
+
+
+
 class CoroExecutor
 {
 public:
@@ -36,36 +70,6 @@ private:
     std::mutex mu;
     std::condition_variable cv;
     bool stop_requested;
-};
-
-
-class LifetimeManagedCoroutine
-{
-public:
-    struct promise_type 
-    {
-        using handle  = std::coroutine_handle<promise_type>;
-        LifetimeManagedCoroutine get_return_object() { return LifetimeManagedCoroutine(handle::from_promise(*this)); };
-        void unhandled_exception() noexcept {};
-        std::suspend_always initial_suspend() noexcept {};
-        void return_void() {};
-
-        // signal to CoroExecutor coroutine is ready for deletion
-        std::suspend_always final_suspend();
-        
-        // assigned by CoroExecutor in add_lifetime_coroutine
-        std::shared_ptr<CoroExecutor> executor;
-    };
-
-
-    LifetimeManagedCoroutine
-    ( promise_type::handle handle
-    )
-    : handle_(handle)
-    {}; 
-
-private:
-    promise_type::handle handle_;
 };
 
 
